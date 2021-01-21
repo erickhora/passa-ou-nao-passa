ad0b8a52924cab4c63acc3a4e89412abb62f8327
Erick Hora <erick.hora@ufpe.br>
2021-01-21 07:30:50 -0300

    - Criação de ambiente, projeto e app.

---
+++ passa_ou_nao_passa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for passa_ou_nao_passa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'passa_ou_nao_passa.settings')
+
+application = get_asgi_application()
---
+++ passa_ou_nao_passa/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for passa_ou_nao_passa project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'passa_ou_nao_passa.settings')
+
+application = get_wsgi_application()
